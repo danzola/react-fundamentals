f60e16dbbe9abd99984cc4e82a90986084c62a88
Diego Anzola <diego_anzola@yahoo.es>
2025-01-22 16:17:32 -0500

    Add AppLifting component for state lifting and update index.js to render it

---
+++ react18-exercises/src/AppLifting.js
@@ -0,0 +1,39 @@
+import * as React from 'react'
+
+function Name({ name, onNameChange }) {
+    return (
+        <div>
+            <label htmlFor="name">Name: </label>
+            <input id="name" value={name} onChange={onNameChange} />
+        </div>
+    )
+}
+
+function FavoriteAnimal({ animal, onAnimalChange }) {
+    return (
+        <div>
+            <label htmlFor="animal">Favorite Animal: </label>
+            <input
+                id="animal" value={animal} onChange={onAnimalChange}
+            />
+        </div>
+    )
+}
+
+function Display({ name, animal }) {
+    return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+}
+
+function AppLifting() {
+    const [animal, setAnimal] = React.useState('')
+    const [name, setName] = React.useState('')
+    return (
+        <form>
+            <Name name={name} onNameChange={event => setName(event.target.value)} />
+            <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)} />
+            <Display name={name} animal={animal} />
+        </form>
+    )
+}
+
+export default AppLifting
--- react18-exercises/src/index.js
+++ react18-exercises/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import AppGreeting from './AppGreeting';
+import AppLifting from './AppLifting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppGreeting />    
+    <AppLifting />    
   </React.StrictMode>
 );
 
