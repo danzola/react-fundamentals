19c728527ef4138a7ef31f520d65ffc7209929a0
Diego Anzola <diego_anzola@yahoo.es>
2025-01-23 17:49:31 -0500

    Refactor AppTicTacToe component: implement state management for squares and add game status calculation

--- react18-exercises/src/AppTicTacToe.js
+++ react18-exercises/src/AppTicTacToe.js
@@ -2,12 +2,23 @@ import * as React from 'react'
 import './tictactoe-styles.css'
 
 function Board() {
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
 
-  function selectSquare(square) {    
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  function selectSquare(square) {
+    if (winner || squares[square]) {
+      return
+    }
+    const squaresCopy = [...squares]
+    squaresCopy[square] = nextValue
+    setSquares(squaresCopy)
   }
 
-  function restart() {    
+  function restart() {
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
@@ -20,7 +31,7 @@ function Board() {
 
   return (
     <div>
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -51,6 +62,38 @@ function Game() {
       </div>
     </div>
   )
+}
+
+function calculateStatus(winner, squares, nextValue) {
+  return winner
+    ? `Winner: ${winner}`
+    : squares.every(Boolean)
+      ? `Scratch: Cat's game`
+      : `Next player: ${nextValue}`
+}
+
+function calculateNextValue(squares) {
+  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
 
 function App() {
